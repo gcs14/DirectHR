5ddec53161b84defceaada983c3fd00093d85a28
Garrett Smith <lamelyte@gmail.com>
2022-12-30 10:54:19 -0600

    included 7 new parameters and their getter & setter methods

--- src/main/java/com/gxcsmith94/employeedirectory/model/Employee.java
+++ src/main/java/com/gxcsmith94/employeedirectory/model/Employee.java
@@ -3,6 +3,7 @@ package com.gxcsmith94.employeedirectory.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 public class Employee {
@@ -13,6 +14,13 @@ public class Employee {
     private String name;
     private String username;
     private String email;
+    private String phoneNumber;
+    private LocalDate birthday;
+    private String position;
+    private String jobType;
+    private int salary;
+    private double hourlyWage;
+    private LocalDate hireDate;
 
     public Long getId() {
         return id;
@@ -44,5 +52,61 @@ public class Employee {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(String jobType) {
+        this.jobType = jobType;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public double getHourlyWage() {
+        return hourlyWage;
+    }
+
+    public void setHourlyWage(double hourlyWage) {
+        this.hourlyWage = hourlyWage;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHiredate(LocalDate hiredate) {
+        this.hireDate = hiredate;
     }
 }
