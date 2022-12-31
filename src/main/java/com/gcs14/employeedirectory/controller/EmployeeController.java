7d06320396ea688167869e41266ddd9321ed76c3
Garrett Smith <lamelyte@gmail.com>
2022-12-31 14:07:00 -0600

    * Used @CrossOrigin to have backend app correspond with frontened react app
    * Implemented a method to recall an employee by id number that will throw a custom exception if id number enter is not in the database

--- src/main/java/com/gcs14/employeedirectory/controller/EmployeeController.java
+++ src/main/java/com/gcs14/employeedirectory/controller/EmployeeController.java
@@ -1,16 +1,14 @@
 package com.gcs14.employeedirectory.controller;
 
+import com.gcs14.employeedirectory.exception.UserNotFoundException;
 import com.gcs14.employeedirectory.model.Employee;
 import com.gcs14.employeedirectory.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class EmployeeController {
 
     @Autowired
@@ -21,8 +19,14 @@ public class EmployeeController {
         return employeeRepository.save(newEmployee);
     }
 
-    @GetMapping("/employee")
+    @GetMapping("/employees")
     List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+
+    @GetMapping("/employee/{id}")
+    Employee getEmployeeById(@PathVariable Long id){
+        return employeeRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
     }
 }
