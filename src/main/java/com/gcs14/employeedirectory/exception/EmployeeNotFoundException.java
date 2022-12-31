e5cc178040b3b2e2a73e6094b850025770dc7369
Garrett Smith <lamelyte@gmail.com>
2022-12-31 14:22:20 -0600

    Rename and refactored

--- src/main/java/com/gcs14/employeedirectory/exception/EmployeeNotFoundException.java
+++ src/main/java/com/gcs14/employeedirectory/exception/EmployeeNotFoundException.java
@@ -1,7 +1,7 @@
 package com.gcs14.employeedirectory.exception;
 
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException(Long id){
         super("Could not find the user with id " + id);
     }
 }
