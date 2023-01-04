e82d2b4ad0f8b4d665464d5e079c6a3824e7506a
Garrett Smith <lamelyte@gmail.com>
2023-01-04 16:18:09 -0600

    Created to serve as a starting point for when the application launches

---
+++ src/main/java/com/gcs14/employeedirectory/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.gcs14.employeedirectory.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @GetMapping("")
+    public ModelAndView home() {
+        ModelAndView mav=new ModelAndView("index");
+        return mav;
+    }
+}
