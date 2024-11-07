39985cb749d38c959eb622b764dcf20672ba52cb
Garrett Smith <lamelyte@gmail.com>
2024-11-07 08:56:33 -0600

    update url to access backend data

--- src/main/frontend/src/pages/Home.js
+++ src/main/frontend/src/pages/Home.js
@@ -9,12 +9,12 @@ export default function Home() {
     const {id} = useParams()
 
     const loadEmployees=async()=>{
-        const result=await axios.get('http://localhost:8080/employees')
+        const result=await axios.get('https://direct-hr.com/employees')
         setEmployees(result.data);
     };
 
     const deleteEmployee = async (id) =>{
-      await axios.delete(`http://localhost:8080/employee/${id}`)
+      await axios.delete(`https://direct-hr.com/employee/${id}`)
       loadEmployees();
     };
 
@@ -35,38 +35,37 @@ export default function Home() {
   return (
     <div className='container'>
         <div className='py-5'>
-        <table className="table border shadow">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Name</th>
-      <th scope="col">Username</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone Number</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    {
-        employees.map((employee, index)=>(
+        {/*<table className="table border shadow">*/}
+        <table className="table table-hover border shadow">
+          <thead>
             <tr>
-                <th scope="row" key={index}>{index+1}</th>
-                <td>{employee.name}</td>
-                <td>{employee.username}</td>
-                <td>{employee.email}</td>
-                <td>{formatPhoneNumber(employee.phoneNumber)}</td>
-                <td>
-                    <Link className='btn btn-primary mx-2' to={`/viewemployee/${employee.id}`}>View</Link>
-                    <Link className='btn btn-outline-primary mx-2' to={`/editemployee/${employee.id}`}>Edit</Link>
-                    <button className="btn btn-danger mx-2"
-                    onClick={()=> deleteEmployee(employee.id)}>Delete</button>
-                </td>
+              <th scope="col" className="id">Id</th>
+              <th scope="col" className="name">Name</th>
+              <th scope="col" className="userName">Username</th>
+              <th scope="col" className="email">Email</th>
+              <th scope="col" className="phoneNumber">Phone Number</th>
+              <th scope="col" className="action">Action</th>
             </tr>
-        ))
-    }
-
-  </tbody>
-</table>
+          </thead>
+          <tbody>
+            {employees.map((employee)=>(
+                <tr>
+                    <th className="employee_id">{employee.id}</th>
+                    <td className="employee_name">{employee.name}</td>
+                    <td className="employee_userName">{employee.username}</td>
+                    <td className="employee_email">{employee.email}</td>
+                    <td className="employee_phoneNumber">{formatPhoneNumber(employee.phoneNumber)}</td>
+                    <td className="employee_actions">
+                        <Link className='btn btn-primary mx-2' to={`/viewemployee/${employee.id}`}>View</Link>
+                        <Link className='btn btn-outline-primary mx-2' to={`/editemployee/${employee.id}`}>Edit</Link>
+                        <button className="btn btn-danger mx-2"
+                        onClick={()=> deleteEmployee(employee.id)}>Delete</button>
+                    </td>
+                </tr>
+            ))
+            }
+          </tbody>
+        </table>
         </div>
     </div>
   )
