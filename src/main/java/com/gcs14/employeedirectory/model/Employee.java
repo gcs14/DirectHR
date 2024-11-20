e1725c432d8a914c3d4bafeff8c1e3982e5c96cd
Garrett Smith <lamelyte@gmail.com>
2024-11-20 11:24:16 -0600

    added constructor

--- src/main/java/com/gcs14/employeedirectory/model/Employee.java
+++ src/main/java/com/gcs14/employeedirectory/model/Employee.java
@@ -1,9 +1,6 @@
 package com.gcs14.employeedirectory.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,4 +25,21 @@ public class Employee {
     private double hourlyWage;
     private LocalDate hireDate;
 
+    public Employee() {
+
+    }
+
+    public Employee(Long id, String name, String username, String email, String phoneNumber, LocalDate birthday, String position, String jobType, int salary, double hourlyWage, LocalDate hireDate) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.birthday = birthday;
+        this.position = position;
+        this.jobType = jobType;
+        this.salary = salary;
+        this.hourlyWage = hourlyWage;
+        this.hireDate = hireDate;
+    }
 }
