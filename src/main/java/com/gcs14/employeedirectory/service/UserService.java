2f907fc0381fd5971978e8ca2b1b6d8ad7d71e22
Garrett Smith <lamelyte@gmail.com>
2024-11-25 15:20:38 -0600

    added jwt authentication

--- src/main/java/com/gcs14/employeedirectory/service/UserService.java
+++ src/main/java/com/gcs14/employeedirectory/service/UserService.java
@@ -1,9 +1,11 @@
 package com.gcs14.employeedirectory.service;
 
-import com.gcs14.employeedirectory.model.Employee;
 import com.gcs14.employeedirectory.model.Users;
 import com.gcs14.employeedirectory.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +15,33 @@ import java.util.List;
 public class UserService {
 
     @Autowired
-    UserRepository repo;
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private JWTService jwtService;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
-    public Users register(Users user){
+    public Users register(Users user) {
         user.setPassword(encoder.encode(user.getPassword()));
-        return repo.save(user);
+        repo.save(user);
+        return user;
     }
 
     public List<Users> getUsers() {
         return repo.findAll();
+    }
+
+    public String verify(Users user) {
+        Authentication authentication =
+                authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if (authentication.isAuthenticated()){
+            return jwtService.generateToken(user.getUsername());
+        }
+        return "fail";
     }
 }
