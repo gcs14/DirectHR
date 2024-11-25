a34daf5ec0cb28539993a1ab97ddf13f2188b73a
Garrett Smith <lamelyte@gmail.com>
2024-11-25 15:19:57 -0600

    create post method to login as verified users

--- src/main/java/com/gcs14/employeedirectory/controller/UserController.java
+++ src/main/java/com/gcs14/employeedirectory/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.gcs14.employeedirectory.controller;
 
-import com.gcs14.employeedirectory.model.Employee;
 import com.gcs14.employeedirectory.model.Users;
 import com.gcs14.employeedirectory.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
@@ -21,8 +19,13 @@ public class UserController {
         return service.register(user);
     }
 
-    @GetMapping("/register")
-    public List<Users> getEmployees() {
+    @PostMapping("/login")
+    public String login(@RequestBody Users user){
+        return service.verify(user);
+    }
+
+    @GetMapping("/users")
+    public List<Users> getUsers(){
         return service.getUsers();
     }
 }
